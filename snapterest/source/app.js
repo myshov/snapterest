adae4f49d9ab15c90deae9f452de212d92efaae9
Alexander Myshov <simplex.light@gmail.com>
2016-02-06 15:51:56 +0600

    Initial commit

---
+++ snapterest/source/app.js
@@ -0,0 +1,36 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var listItemElement1 = React.DOM.li({
+    className: 'item-1',
+    key: 'item-1'
+});
+
+var listItemElement2 = React.DOM.li({
+    className: 'item-2',
+    key: 'item-2'
+});
+
+var listItemElement3 = React.DOM.li({
+    className: 'item-3',
+    key: 'item-3'
+});
+
+var reactFragment = [
+    listItemElement1,
+    listItemElement2,
+    listItemElement3
+];
+
+var listOfItems = React.DOM.ul(
+    {
+        className: 'lsit-of-items',
+    },
+    reactFragment
+);
+
+ReactDOM.render(
+    listOfItems,
+    document.getElementById('react-application')
+);
+
