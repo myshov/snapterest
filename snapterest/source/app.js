e4076664d5529aa1016c9c763eb7dc9274c865e5
Alexander Myshov <simplex.light@gmail.com>
2016-02-07 23:17:26 +0600

    Create simple stateless component

--- snapterest/source/app.js
+++ snapterest/source/app.js
@@ -3,15 +3,23 @@ var ReactDOM = require('react-dom');
 
 var ReactClass = React.createClass({
     render: function() {
+        if (this.props.isHidden) {
+            return null;
+        }
+
         return React.createElement(
             'h1',
             {className: 'header'},
-            'React Component'
+            this.props.header
         );
     }
 });
 
-var reactComponentElement = React.createElement(ReactClass);
+var reactComponentElement = React.createElement(ReactClass, {
+    isHidden: false,
+    header: 'Some Caption Here'
+});
+
 var reactComponent = ReactDOM.render(
     reactComponentElement,
     document.getElementById('react-application')
