387e7dd577392a7787b6052f7084bb90c51fa87d
Alexander Myshov <simplex.light@gmail.com>
2016-02-07 21:30:02 +0600

    change to jsx syntax

--- snapterest/source/app.js
+++ snapterest/source/app.js
@@ -1,38 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ReactDOMServer = require('react-dom/server');
 
-var listItemElement1 = React.DOM.li({
-    className: 'item-1',
-    key: 'item-1'
-}, 'Item 1');
-
-var listItemElement2 = React.DOM.li({
-    className: 'item-2',
-    key: 'item-2'
-}, 'Item 2');
-
-var listItemElement3 = React.DOM.li({
-    className: 'item-3',
-    key: 'item-3'
-}, 'Item 3');
-
-var reactFragment = [
-    listItemElement1,
-    listItemElement2,
-    listItemElement3
-];
-
-var listOfItems = React.DOM.ul(
-    {
-        className: 'lsit-of-items',
-    },
-    reactFragment
-);
+var listOfItems = 
+    <ul className="list-of-items">
+        <li className="item-1" key="item-1">Item 1</li>
+        <li className="item-2" key="item-2">Item 2</li>
+        <li className="item-3" key="item-3">Item 3</li>
+    </ul>;
 
 ReactDOM.render(
     listOfItems,
     document.getElementById('react-application')
 );
-
-console.log(ReactDOMServer.renderToStaticMarkup(listOfItems));
