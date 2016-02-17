9b2a5179d5aec5cb1bd168c64220b463ad08cd57
Alexander Myshov <simplex.light@gmail.com>
2016-02-18 02:48:04 +0600

    Create full application

--- snapterest/source/app.js
+++ snapterest/source/app.js
@@ -1,60 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Application = require('./components/Application.react')
 
-var ReactClass = React.createClass({
-    getInitialState: function() {
-        return {
-            isHeaderHidden: false
-        }
-    },
-
-    handleClick: function() {
-        this.setState({
-            isHeaderHidden: !this.state.isHeaderHidden
-        })
-    },
-
-    render: function() {
-        var title = 'Stateful React Component';
-
-        var headerElement = React.createElement(
-            'h1',
-            {className: 'header', key: 'header'},
-            title
-        );
-
-        var buttonElement = React.createElement(
-            'button',
-            {
-                className: 'btn btn-default',
-                onClick: this.handleClick,
-                key: 'button'
-            },
-            'Toggle header'
-        );
-
-        if (this.state.isHeaderHidden) {
-            return React.createElement(
-                'div',
-                null,
-                [buttonElement]
-            );
-        }
-
-        return React.createElement(
-            'div',
-            null,
-            [buttonElement, headerElement]
-        );
-    }
-});
-
-var reactComponentElement = React.createElement(ReactClass, {
-    isHidden: false,
-    header: 'Some Caption Here'
-});
-
-var reactComponent = ReactDOM.render(
-    reactComponentElement,
+ReactDOM.render(
+    <Application />,
     document.getElementById('react-application')
 );
